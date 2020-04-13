2f6a13ef4e126562582523ed5b9cdab7bea3140a
Akatsuki1910 <35941346+Akatsuki1910@users.noreply.github.com>
2020-04-13 17:22:40 +0900

    beautiful code

--- src/index.ts
+++ src/index.ts
@@ -6,77 +6,191 @@ import {
 	VRM,
 	VRMSchema
 } from '@pixiv/three-vrm'
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
-import { DeviceOrientationControls } from "three/examples/jsm/controls/DeviceOrientationControls"
+import {
+	OrbitControls
+} from "three/examples/jsm/controls/OrbitControls"
+import {
+	DeviceOrientationControls
+} from "three/examples/jsm/controls/DeviceOrientationControls"
 import Stats from 'stats.js';
 import * as nipplejs from 'nipplejs'
 import * as PIXI from 'pixi.js'
 
 // import {bonevpd} from "./pose";
 
-function returnBone(vrm:any,boneSt:String){
+function returnBone(vrm: any, boneSt: String) {
 	let bone;
-	switch(boneSt){
-		case "Chest": bone = VRMSchema.HumanoidBoneName.Chest; break;
-		case "Head": bone = VRMSchema.HumanoidBoneName.Head; break;
-		case "Hips": bone = VRMSchema.HumanoidBoneName.Hips; break;
-		case "Jaw": bone = VRMSchema.HumanoidBoneName.Jaw; break;
-		case "LeftEye": bone = VRMSchema.HumanoidBoneName.LeftEye; break;
-		case "LeftFoot": bone = VRMSchema.HumanoidBoneName.LeftFoot; break;
-		case "LeftHand": bone = VRMSchema.HumanoidBoneName.LeftHand; break;
-		case "LeftIndexDistal": bone = VRMSchema.HumanoidBoneName.LeftIndexDistal; break;
-		case "LeftIndexIntermediate": bone = VRMSchema.HumanoidBoneName.LeftIndexIntermediate; break;
-		case "LeftIndexProximal": bone = VRMSchema.HumanoidBoneName.LeftIndexProximal; break;
-		case "LeftLittleDistal": bone = VRMSchema.HumanoidBoneName.LeftLittleDistal; break;
-		case "LeftLittleIntermediate": bone = VRMSchema.HumanoidBoneName.LeftLittleIntermediate; break;
-		case "LeftLittleProximal": bone = VRMSchema.HumanoidBoneName.LeftLittleProximal; break;
-		case "LeftLowerArm": bone = VRMSchema.HumanoidBoneName.LeftLowerArm; break;
-		case "LeftLowerLeg": bone = VRMSchema.HumanoidBoneName.LeftLowerLeg; break;
-		case "LeftMiddleDistal": bone = VRMSchema.HumanoidBoneName.LeftMiddleDistal; break;
-		case "LeftMiddleIntermediate": bone = VRMSchema.HumanoidBoneName.LeftMiddleIntermediate; break;
-		case "LeftMiddleProximal": bone = VRMSchema.HumanoidBoneName.LeftMiddleProximal; break;
-		case "LeftRingDistal": bone = VRMSchema.HumanoidBoneName.LeftRingDistal; break;
-		case "LeftRingIntermediate": bone = VRMSchema.HumanoidBoneName.LeftRingIntermediate; break;
-		case "LeftRingProximal": bone = VRMSchema.HumanoidBoneName.LeftRingProximal; break;
-		case "LeftShoulder": bone = VRMSchema.HumanoidBoneName.LeftShoulder; break;
-		case "LeftThumbDistal": bone = VRMSchema.HumanoidBoneName.LeftThumbDistal; break;
-		case "LeftThumbIntermediate": bone = VRMSchema.HumanoidBoneName.LeftThumbIntermediate; break;
-		case "LeftThumbProximal": bone = VRMSchema.HumanoidBoneName.LeftThumbProximal; break;
-		case "LeftToes": bone = VRMSchema.HumanoidBoneName.LeftToes; break;
-		case "LeftUpperArm": bone = VRMSchema.HumanoidBoneName.LeftUpperArm; break;
-		case "LeftUpperLeg": bone = VRMSchema.HumanoidBoneName.LeftUpperLeg; break;
-		case "Neck": bone = VRMSchema.HumanoidBoneName.Neck; break;
-		case "RightEye": bone = VRMSchema.HumanoidBoneName.RightEye; break;
-		case "RightFoot": bone = VRMSchema.HumanoidBoneName.RightFoot; break;
-		case "RightHand": bone = VRMSchema.HumanoidBoneName.RightHand; break;
-		case "RightIndexDistal": bone = VRMSchema.HumanoidBoneName.RightIndexDistal; break;
-		case "RightIndexIntermediate": bone = VRMSchema.HumanoidBoneName.RightIndexIntermediate; break;
-		case "RightIndexProximal": bone = VRMSchema.HumanoidBoneName.RightIndexProximal; break;
-		case "RightLittleDistal": bone = VRMSchema.HumanoidBoneName.RightLittleDistal; break;
-		case "RightLittleIntermediate": bone = VRMSchema.HumanoidBoneName.RightLittleIntermediate; break;
-		case "RightLittleProximal": bone = VRMSchema.HumanoidBoneName.RightLittleProximal; break;
-		case "RightLowerArm": bone = VRMSchema.HumanoidBoneName.RightLowerArm; break;
-		case "RightLowerLeg": bone = VRMSchema.HumanoidBoneName.RightLowerLeg; break;
-		case "RightMiddleDistal": bone = VRMSchema.HumanoidBoneName.RightMiddleDistal; break;
-		case "RightMiddleIntermediate": bone = VRMSchema.HumanoidBoneName.RightMiddleIntermediate; break;
-		case "RightMiddleProximal": bone = VRMSchema.HumanoidBoneName.RightMiddleProximal; break;
-		case "RightRingDistal": bone = VRMSchema.HumanoidBoneName.RightRingDistal; break;
-		case "RightRingIntermediate": bone = VRMSchema.HumanoidBoneName.RightRingIntermediate; break;
-		case "RightRingProximal": bone = VRMSchema.HumanoidBoneName.RightRingProximal; break;
-		case "RightShoulder": bone = VRMSchema.HumanoidBoneName.RightShoulder; break;
-		case "RightThumbDistal": bone = VRMSchema.HumanoidBoneName.RightThumbDistal; break;
-		case "RightThumbIntermediate": bone = VRMSchema.HumanoidBoneName.RightThumbIntermediate; break;
-		case "RightThumbProximal": bone = VRMSchema.HumanoidBoneName.RightThumbProximal; break;
-		case "RightToes": bone = VRMSchema.HumanoidBoneName.RightToes; break;
-		case "RightUpperArm": bone = VRMSchema.HumanoidBoneName.RightUpperArm; break;
-		case "RightUpperLeg": bone = VRMSchema.HumanoidBoneName.RightUpperLeg; break;
-		case "Spine": bone = VRMSchema.HumanoidBoneName.Spine; break;
-		case "UpperChest": bone = VRMSchema.HumanoidBoneName.UpperChest; break;
+	switch (boneSt) {
+		case "Chest":
+			bone = VRMSchema.HumanoidBoneName.Chest;
+			break;
+		case "Head":
+			bone = VRMSchema.HumanoidBoneName.Head;
+			break;
+		case "Hips":
+			bone = VRMSchema.HumanoidBoneName.Hips;
+			break;
+		case "Jaw":
+			bone = VRMSchema.HumanoidBoneName.Jaw;
+			break;
+		case "LeftEye":
+			bone = VRMSchema.HumanoidBoneName.LeftEye;
+			break;
+		case "LeftFoot":
+			bone = VRMSchema.HumanoidBoneName.LeftFoot;
+			break;
+		case "LeftHand":
+			bone = VRMSchema.HumanoidBoneName.LeftHand;
+			break;
+		case "LeftIndexDistal":
+			bone = VRMSchema.HumanoidBoneName.LeftIndexDistal;
+			break;
+		case "LeftIndexIntermediate":
+			bone = VRMSchema.HumanoidBoneName.LeftIndexIntermediate;
+			break;
+		case "LeftIndexProximal":
+			bone = VRMSchema.HumanoidBoneName.LeftIndexProximal;
+			break;
+		case "LeftLittleDistal":
+			bone = VRMSchema.HumanoidBoneName.LeftLittleDistal;
+			break;
+		case "LeftLittleIntermediate":
+			bone = VRMSchema.HumanoidBoneName.LeftLittleIntermediate;
+			break;
+		case "LeftLittleProximal":
+			bone = VRMSchema.HumanoidBoneName.LeftLittleProximal;
+			break;
+		case "LeftLowerArm":
+			bone = VRMSchema.HumanoidBoneName.LeftLowerArm;
+			break;
+		case "LeftLowerLeg":
+			bone = VRMSchema.HumanoidBoneName.LeftLowerLeg;
+			break;
+		case "LeftMiddleDistal":
+			bone = VRMSchema.HumanoidBoneName.LeftMiddleDistal;
+			break;
+		case "LeftMiddleIntermediate":
+			bone = VRMSchema.HumanoidBoneName.LeftMiddleIntermediate;
+			break;
+		case "LeftMiddleProximal":
+			bone = VRMSchema.HumanoidBoneName.LeftMiddleProximal;
+			break;
+		case "LeftRingDistal":
+			bone = VRMSchema.HumanoidBoneName.LeftRingDistal;
+			break;
+		case "LeftRingIntermediate":
+			bone = VRMSchema.HumanoidBoneName.LeftRingIntermediate;
+			break;
+		case "LeftRingProximal":
+			bone = VRMSchema.HumanoidBoneName.LeftRingProximal;
+			break;
+		case "LeftShoulder":
+			bone = VRMSchema.HumanoidBoneName.LeftShoulder;
+			break;
+		case "LeftThumbDistal":
+			bone = VRMSchema.HumanoidBoneName.LeftThumbDistal;
+			break;
+		case "LeftThumbIntermediate":
+			bone = VRMSchema.HumanoidBoneName.LeftThumbIntermediate;
+			break;
+		case "LeftThumbProximal":
+			bone = VRMSchema.HumanoidBoneName.LeftThumbProximal;
+			break;
+		case "LeftToes":
+			bone = VRMSchema.HumanoidBoneName.LeftToes;
+			break;
+		case "LeftUpperArm":
+			bone = VRMSchema.HumanoidBoneName.LeftUpperArm;
+			break;
+		case "LeftUpperLeg":
+			bone = VRMSchema.HumanoidBoneName.LeftUpperLeg;
+			break;
+		case "Neck":
+			bone = VRMSchema.HumanoidBoneName.Neck;
+			break;
+		case "RightEye":
+			bone = VRMSchema.HumanoidBoneName.RightEye;
+			break;
+		case "RightFoot":
+			bone = VRMSchema.HumanoidBoneName.RightFoot;
+			break;
+		case "RightHand":
+			bone = VRMSchema.HumanoidBoneName.RightHand;
+			break;
+		case "RightIndexDistal":
+			bone = VRMSchema.HumanoidBoneName.RightIndexDistal;
+			break;
+		case "RightIndexIntermediate":
+			bone = VRMSchema.HumanoidBoneName.RightIndexIntermediate;
+			break;
+		case "RightIndexProximal":
+			bone = VRMSchema.HumanoidBoneName.RightIndexProximal;
+			break;
+		case "RightLittleDistal":
+			bone = VRMSchema.HumanoidBoneName.RightLittleDistal;
+			break;
+		case "RightLittleIntermediate":
+			bone = VRMSchema.HumanoidBoneName.RightLittleIntermediate;
+			break;
+		case "RightLittleProximal":
+			bone = VRMSchema.HumanoidBoneName.RightLittleProximal;
+			break;
+		case "RightLowerArm":
+			bone = VRMSchema.HumanoidBoneName.RightLowerArm;
+			break;
+		case "RightLowerLeg":
+			bone = VRMSchema.HumanoidBoneName.RightLowerLeg;
+			break;
+		case "RightMiddleDistal":
+			bone = VRMSchema.HumanoidBoneName.RightMiddleDistal;
+			break;
+		case "RightMiddleIntermediate":
+			bone = VRMSchema.HumanoidBoneName.RightMiddleIntermediate;
+			break;
+		case "RightMiddleProximal":
+			bone = VRMSchema.HumanoidBoneName.RightMiddleProximal;
+			break;
+		case "RightRingDistal":
+			bone = VRMSchema.HumanoidBoneName.RightRingDistal;
+			break;
+		case "RightRingIntermediate":
+			bone = VRMSchema.HumanoidBoneName.RightRingIntermediate;
+			break;
+		case "RightRingProximal":
+			bone = VRMSchema.HumanoidBoneName.RightRingProximal;
+			break;
+		case "RightShoulder":
+			bone = VRMSchema.HumanoidBoneName.RightShoulder;
+			break;
+		case "RightThumbDistal":
+			bone = VRMSchema.HumanoidBoneName.RightThumbDistal;
+			break;
+		case "RightThumbIntermediate":
+			bone = VRMSchema.HumanoidBoneName.RightThumbIntermediate;
+			break;
+		case "RightThumbProximal":
+			bone = VRMSchema.HumanoidBoneName.RightThumbProximal;
+			break;
+		case "RightToes":
+			bone = VRMSchema.HumanoidBoneName.RightToes;
+			break;
+		case "RightUpperArm":
+			bone = VRMSchema.HumanoidBoneName.RightUpperArm;
+			break;
+		case "RightUpperLeg":
+			bone = VRMSchema.HumanoidBoneName.RightUpperLeg;
+			break;
+		case "Spine":
+			bone = VRMSchema.HumanoidBoneName.Spine;
+			break;
+		case "UpperChest":
+			bone = VRMSchema.HumanoidBoneName.UpperChest;
+			break;
 	}
 	return vrm.humanoid!.getBoneNode(bone) !;
 }
 
-const ua:string[] = [
+const ua: string[] = [
 	"iPod",
 	"iPad",
 	"iPhone"
@@ -93,10 +207,10 @@ function failure() {
 
 
 (document.getElementById("test") as HTMLElement).innerHTML = window.navigator.userAgent;
-let iosflg:boolean = false;
+let iosflg: boolean = false;
 if (((window.DeviceOrientationEvent) && ('ontouchstart' in window))) {
 	//mobile
-	for (let i:number = 0; i < ua.length; i++) {
+	for (let i: number = 0; i < ua.length; i++) {
 		if (window.navigator.userAgent.indexOf(ua[i]) > 0) {
 			iosflg = true;
 			success();
@@ -141,27 +255,25 @@ function check() {
 
 const xzManager = nipplejs.create({
 	zone: document.getElementById("xzBox") as HTMLElement,
-	// mode: 'static',
-	// position: {left: '50%', top: '50%'},
 	color: 'white'
 });
 
 const yManager = nipplejs.create({
 	zone: document.getElementById("yBox") as HTMLElement,
-	// mode: 'static',
-	// position: {left: '50%', top: '50%'},
 	color: 'red',
-	lockY:true
+	lockY: true
 });
 
-function main(){
+function main() {
 	(document.getElementById("canvas-wrapper-main") as HTMLElement).style.display = "inline";
 	(document.getElementById("title") as HTMLElement).style.display = "none";
+	(document.getElementById("xzBox") as HTMLElement).style.top = String(window.innerHeight - window.innerWidth / 2) + "px";
+	(document.getElementById("yBox") as HTMLElement).style.top = String(window.innerHeight - window.innerWidth / 2) + "px";
 	// document.body.requestFullscreen();//ios非対応
 
 	window.resizeTo(window.innerWidth, window.innerHeight);
-	const width:number = window.innerWidth;
-	const height:number = window.innerHeight;
+	const width: number = window.innerWidth;
+	const height: number = window.innerHeight;
 
 	const stats = new Stats();
 	stats.showPanel(0);
@@ -178,30 +290,30 @@ function main(){
 	(document.getElementById("pixiview") as HTMLElement).appendChild(renderer.view);
 
 	//xz軸
-	xzManager.on("move",(e,n)=>{
-		let deg_see:number = n.angle.degree;
-		if(deg_see<=270){
-			deg_see-=90;
-		}else{
-			deg_see-=360+90;
+	xzManager.on("move", (e, n) => {
+		let deg_see: number = n.angle.degree;
+		if (deg_see <= 270) {
+			deg_see -= 90;
+		} else {
+			deg_see -= 360 + 90;
 		}
-		deg_see = Math.floor(deg_see * 1000)/1000;//小数3桁まで
+		deg_see = Math.floor(deg_see * 1000) / 1000; //小数3桁まで
 
-		let deg_move:number = (camera.rotation.y * 180 / Math.PI) + deg_see;
-		if(deg_move<-90){
-			deg_move+=360+90;
-		}else{
-			deg_move+=90;
+		let deg_move: number = (camera.rotation.y * 180 / Math.PI) + deg_see;
+		if (deg_move < -90) {
+			deg_move += 360 + 90;
+		} else {
+			deg_move += 90;
 		}
-		camera.position.x += Math.cos(deg_move*(Math.PI / 180)) * 0.01;
-		camera.position.z -= Math.sin(deg_move*(Math.PI / 180)) * 0.01;
+		camera.position.x += Math.cos(deg_move * (Math.PI / 180)) * 0.01;
+		camera.position.z -= Math.sin(deg_move * (Math.PI / 180)) * 0.01;
 	});
 
 	//y軸
-	yManager.on("move",(e,n)=>{
-		if(n.direction!=undefined){
-			const force:number = n.force;
-			const p:number = (n.direction.y=="up")?1:-1;
+	yManager.on("move", (e, n) => {
+		if (n.direction != undefined) {
+			const force: number = n.force;
+			const p: number = (n.direction.y == "up") ? 1 : -1;
 			camera.position.y += force * 0.01 * p;
 		}
 	});
@@ -214,7 +326,7 @@ function main(){
 		0.1,
 		1000
 	);
-	const cameraY:number = 0.8;
+	const cameraY: number = 0.8;
 	camera.position.set(0, cameraY, 2);
 
 	const controls = new DeviceOrientationControls(camera);
@@ -238,24 +350,24 @@ function main(){
 	light.position.set(1, 1, 1).normalize();
 	scene.add(light);
 
-	let geometry = new THREE.SphereGeometry(10);
-	let material = new THREE.MeshBasicMaterial({
-		vertexColors: THREE.FaceColors as any
-	});
-	for (let l:number = 0; l < geometry.faces.length; l++) {
-		geometry.faces[l].color.set(Math.random() * 0xCC0000);
-	}
+	// let geometry = new THREE.SphereGeometry(10);
+	// let material = new THREE.MeshBasicMaterial({
+	// vertexColors: THREE.FaceColors as any
+	// });
+	// for (let l:number = 0; l < geometry.faces.length; l++) {
+	// geometry.faces[l].color.set(Math.random() * 0xCC0000);
+	// }
 
-	let box:any[] = [];
-	let num:number = 4000;
+	// let box:any[] = [];
+	// let num:number = 4000;
 
-	for (let i:number = 0; i < 5000; i++) {
-		box[i] = new THREE.Mesh(geometry, material);
-		box[i].position.set((Math.random() * num) - num / 2, (Math.random() * num) - num / 2, (Math.random() * num) - num / 2);
-		scene.add(box[i]);
-	}
+	// for (let i:number = 0; i < 5000; i++) {
+	// box[i] = new THREE.Mesh(geometry, material);
+	// box[i].position.set((Math.random() * num) - num / 2, (Math.random() * num) - num / 2, (Math.random() * num) - num / 2);
+	// scene.add(box[i]);
+	// }
 
-	let vrmupdate:any;
+	let vrmupdate: any;
 
 	const loader = new GLTFLoader();
 
@@ -269,6 +381,9 @@ function main(){
 				scene.add(vrm.scene);
 				vrm.scene.rotation.y = Math.PI;
 				vrmupdate = vrm;
+
+				//ここが一番重い
+				(document.getElementById('loadingWrapper') as HTMLElement).classList.add('loaded');
 
 				// bonevpd(vrm,VRMSchema);
 
@@ -289,12 +404,12 @@ function main(){
 	const update = () => {
 		requestAnimationFrame(update);
 		stats.begin();
-		for (let i:number = 0; i < box.length; i++) {
-			box[i].rotation.y += 0.1;
-		}
+		// for (let i:number = 0; i < box.length; i++) {
+		// box[i].rotation.y += 0.1;
+		// }
 		stats.end();
 		//初期カメラ調整
-		if(controls.alphaOffset==0 && camera.rotation.y!=0){
+		if (controls.alphaOffset == 0 && camera.rotation.y != 0) {
 			controls.alphaOffset = camera.rotation.y * -1;
 		}
 		controls.update();
